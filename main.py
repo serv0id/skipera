b9e950d01886fd0160846dcd56c2400ed55da59a
serv0id <113516593+serv0id@users.noreply.github.com>
2023-04-04 07:17:55 +0530

    added skeleton class

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-import requests
+import requests, sys
 import config
-import loguru
+from loguru import logger
 
 class Skipera(object):
-    def __init__(self):
+    def __init__(self, course):
+        self.base_url = config.BASE_URL
+        self.session = requests.Session()
+        self.session.headers.update(config.HEADERS)
+        self.session.cookies.update(config.COOKIES)
+        self.course = course
+    
+    def get_modules(self):
+        r = self.session.get(self.base_url
+         + f"onDemandCourseMaterials.v2/?q=slug&slug={self.course}&includes=modules").json()
+        logger.info(r)
 
-response = requests.get(
-    'https://www.coursera.org/api/onDemandCourseMaterials.v2/?q=slug&slug=introduction-psychology&includes=modules&showLockedItems=true',
-    cookies=config.COOKIES,
-    headers=config.HEADERS,
-)
-print(response.content)
+@logger.catch
+def main():
+    skipera = Skipera("introduction-psychology")
+    skipera.get_modules()
+if __name__ == '__main__':
+    main()

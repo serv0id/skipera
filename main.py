1caf0d153fab4410a581d4245efc6311dfb0dfc7
painaddict <115467129+painaddict@users.noreply.github.com>
2023-04-04 11:09:50 +0530

    added stuff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests, sys
 import config
 from loguru import logger
+import click
 
 class Skipera(object):
     def __init__(self, course):
@@ -9,15 +10,29 @@ class Skipera(object):
         self.session.headers.update(config.HEADERS)
         self.session.cookies.update(config.COOKIES)
         self.course = course
-    
+        self.get_userid()
+
+    def get_userid(self):
+        r = self.session.get(self.base_url + "adminUserPermissions.v1?q=my").json()
+        self.user_id = r["elements"][0]["id"]
+        logger.info("User ID: " + self.user_id)
+
+
     def get_modules(self):
         r = self.session.get(self.base_url
          + f"onDemandCourseMaterials.v2/?q=slug&slug={self.course}&includes=modules").json()
-        logger.info(r)
+        self.course_id = r["elements"][0]["id"]
+        logger.debug("Course ID: " + self.course_id)
+        logger.debug("Number of Modules: " + str(len(r["linked"]["onDemandCourseMaterialModules.v1"])))
+        for x in r["linked"]["onDemandCourseMaterialModules.v1"]:
+            logger.info(x["name"] + " -- " + x["id"])
+
+    def watch_module(self, lesson_id):
+        r = self.session.post(self.base_url + f"opencourse.v1/user/{self.user_id}/course/{self.course}/item/0Wmh7/lecture/videoEvents/ended?autoEnroll=false")
 
 @logger.catch
 def main():
-    skipera = Skipera("introduction-psychology")
+    skipera = Skipera(sys.argv[1])
     skipera.get_modules()
 if __name__ == '__main__':
     main()

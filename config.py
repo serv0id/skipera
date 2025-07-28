5b59f774f7be5dc2f468f31c97d40149b461d4e5
serv0id <113516593+serv0id@users.noreply.github.com>
2025-07-28 07:33:52 +0530

    Config file added

---
+++ config.py
@@ -0,0 +1,25 @@
+# https://github.com/serv0id/skipera
+
+BASE_URL = "https://www.coursera.org/api/"
+GRAPHQL_URL = "https://www.coursera.org/graphql-gateway"
+
+PERPLEXITY_API_URL = "https://api.perplexity.ai/chat/completions"
+PERPLEXITY_MODEL = "sonar"  # adjust this according to your preference
+
+HEADERS = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36',
+    'x-coursera-application': 'ondemand',
+    'x-coursera-version': '3bfd497de04ae0fef167b747fd85a6fbc8fb55df',
+    'x-requested-with': 'XMLHttpRequest',
+}
+
+COOKIES = {
+
+}
+
+# Credentials
+
+EMAIL = ''
+PASSWORD = ''
+
+PERPLEXITY_API_KEY = ""
